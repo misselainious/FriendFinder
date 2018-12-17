f1ab49e8150fb659b02a5e38cf10b4d3c0c2c675
Elaine Cadman <elaine.cadman@gmail.com>
2018-12-17 12:16:00 -0800

    routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,5 +1,6 @@
 var path = require("path");
 
+
 module.exports = function(app){
 
 //Routes for home and survey pages
@@ -10,4 +11,8 @@ app.get("/", function(req, res) {
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
-}
+
+  app.get("*", function(req, res){
+      res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+};
